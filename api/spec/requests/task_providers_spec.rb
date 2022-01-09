cbe82c9d71df31b2a753b8e29eca6fbe44c60153
yuya-okada527 <yuya.okada527@gmail.com>
2022-01-09 19:36:39 +0900

    add delete api request spec

--- api/spec/requests/task_providers_spec.rb
+++ api/spec/requests/task_providers_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'TaskProvidersAPI' do
   let (:project) { create(:project) }
   describe '/v1/projects/{project_id}/task_providers' do
     describe 'GET' do
-      let (:task_provider) { create(:task_provider) }
+      let (:task_provider) { create(:task_provider, project_id: project.id) }
       it 'status code is 200' do
         get "/v1/projects/#{project.id}/task_providers"
         expect(response.status).to eq 200
@@ -23,13 +23,22 @@ RSpec.describe 'TaskProvidersAPI' do
     end
   end
   describe '/v1/projects/{project_id}/task_providers/{task_provider_id}' do
+    let (:task_provider) { create(:task_provider, project_id: project.id) }
     describe 'DELETE' do
+      it 'status code is 200' do
+        delete "/v1/projects/#{project.id}/task_providers/#{task_provider.id}"
+        expect(response.status).to eq 200
+      end
+      xit 'properly deleted?' do
 
+      end
     end
   end
   describe '/v1/projects/{project_id}/task_providers/{task_provider_id}/sync' do
     describe 'POST' do
+      xit 'syncは、外部APIをコールするので、テスト方法を検討' do
 
+      end
     end
   end
 end
