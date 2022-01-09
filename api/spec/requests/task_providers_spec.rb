ac22986038ce550cf044de62470473a8070a0976
yuya-okada527 <yuya.okada527@gmail.com>
2022-01-09 19:24:32 +0900

    add task providers get api request spec

--- api/spec/requests/task_providers_spec.rb
+++ api/spec/requests/task_providers_spec.rb
@@ -4,7 +4,11 @@ RSpec.describe 'TaskProvidersAPI' do
   let (:project) { create(:project) }
   describe '/v1/projects/{project_id}/task_providers' do
     describe 'GET' do
-
+      let (:task_provider) { create(:task_provider) }
+      it 'status code is 200' do
+        get "/v1/projects/#{project.id}/task_providers"
+        expect(response.status).to eq 200
+      end
     end
     describe 'POST' do
 
