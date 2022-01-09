90605d2b41a05b61ec4bd428ae08f51d85c16f56
yuya-okada527 <yuya.okada527@gmail.com>
2022-01-09 19:15:26 +0900

    add task providers api request spec describe

---
+++ api/spec/requests/task_providers_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'TaskProvidersAPI' do
+  let (:project) { create(:project) }
+  describe '/v1/projects/{project_id}/task_providers' do
+    describe 'GET' do
+
+    end
+    describe 'POST' do
+
+    end
+  end
+  describe '/v1/projects/{project_id}/task_providers/{task_provider_id}' do
+    describe 'DELETE' do
+
+    end
+  end
+  describe '/v1/projects/{project_id}/task_providers/{task_provider_id}/sync' do
+    describe 'POST' do
+
+    end
+  end
+end
