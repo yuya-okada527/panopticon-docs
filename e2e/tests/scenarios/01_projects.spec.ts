2ef320cd63b86dc83ae4db4723eb2b52e6f17c72
yuya-okada527 <yuya.okada527@gmail.com>
2022-01-14 08:39:32 +0900

    fix e2e test

--- e2e/tests/scenarios/01_projects.spec.ts
+++ e2e/tests/scenarios/01_projects.spec.ts
@@ -11,13 +11,14 @@ test.describe("Projects", () => {
       const projectsPage = new ProjectsPage(page);
       await projectsPage.goto();
     });
-    test.only("プロジェクトTOPに遷移できること", async ({ page }) => {
+    test("プロジェクトTOPに遷移できること", async ({ page }) => {
       await page.locator("text='Local Project'").click();
       await expect(page).toHaveURL(/.*\/[0-9]+\/tasks/);
     });
     test("プロジェクトを追加できること", async ({ page }) => {
-      await E2EUtils.sleep(1);
+      await E2EUtils.sleep(2);
       await page.locator("input[type=text]").fill("E2E Project");
+      await E2EUtils.sleep(2);
       await page.locator("text=Add").click();
       await expect(page.locator("a:has-text('E2E Project')")).toBeVisible();
     });
