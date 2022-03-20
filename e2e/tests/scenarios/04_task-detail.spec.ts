944ae377dcc294b6dd89aff6a5b99dcf437d17ec
yuya-okada527 <yuya.okada527@gmail.com>
2022-03-20 20:52:14 +0900

    add task detail specs

--- e2e/tests/scenarios/04_task-detail.spec.ts
+++ e2e/tests/scenarios/04_task-detail.spec.ts
@@ -2,14 +2,19 @@
   タスク詳細画面に対するテストを記載する
  */
 import { test, expect } from "@playwright/test";
+import TaskDetailPage from "../po/task-detail-page";
 import { E2EUtils } from "../utils/e2e-utils";
+
+const projectId = 1;
+const taskId = 1;
 
 test.describe("Task Detail", () => {
   E2EUtils.addCommonHooks(test);
-  test.skip("タスクを編集できる", async ({ page }) => {
-    // TODO implements
-    // タスクステータスを更新できる
-    // タスク概要を更新できる
-    // previewを切り替えられる
+  test.beforeEach(async ({ page }) => {
+    const taskDetailPage = new TaskDetailPage(page);
+    await taskDetailPage.goto(projectId, taskId);
+  });
+  test.only("タスクを編集できる", async ({ page }) => {
+    const taskDetailPage = new TaskDetailPage(page);
   });
 });
